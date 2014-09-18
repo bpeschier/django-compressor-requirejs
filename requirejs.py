90b2ee01d917fdb81bd70362ec8253282627a2f1
Bas Peschier <basp@fabrique.nl>
2014-09-18 22:09:12 +0200

    Get bundles, but no compression yet

--- requirejs.py
+++ requirejs.py
@@ -1,8 +1,8 @@
 from itertools import chain
-import json
 import os
 import re
 import json
+from compressor.js import JsCompressor
 
 from django.conf import settings
 from django.template.loaders.app_directories import app_template_dirs
@@ -14,6 +14,7 @@ from compressor.filters.base import FilterBase
 
 require_pattern = re.compile(r'require\((\[.*\])')
 define_pattern = re.compile(r'define\((\[.*\])')
+define_replace_pattern = re.compile(r'define\((.*)\)')
 
 
 class RequireJSCompiler(FilterBase):
@@ -43,11 +44,11 @@ class RequireJSCompiler(FilterBase):
 
     def get_template_dependencies(self):
         for template in self.get_template_files():
-            f = open(template, 'r')
-            matches = require_pattern.findall(f.read(), re.MULTILINE)
-            for match in matches:
-                for dep in self.get_dependencies_from_match(match):
-                    yield dep
+            with open(template, 'r') as f:
+                matches = require_pattern.findall(f.read(), re.MULTILINE)
+                for match in matches:
+                    for dep in self.get_dependencies_from_match(match):
+                        yield dep
 
     @staticmethod
     def find_module(name):
@@ -61,18 +62,34 @@ class RequireJSCompiler(FilterBase):
             path = finders.find('/'.join(module_parts))
         return path
 
-    def resolve_dependencies(self, modules, known):
+    def resolve_dependencies(self, modules, known=None):
+        if known is None:
+            known = set()
         for module in [m for m in modules if m not in known]:
             path = self.find_module(module)
             if path:
                 known.add(module)
-                known.update(self.resolve_dependencies(self.get_module_dependencies(path), known))
+                known.update(self.resolve_dependencies(self.get_module_dependencies(path), known=known))
         return known
+
+    def get_bundle_module(self, module):
+        with open(self.find_module(module), 'r') as f:
+            content = f.read()
+            return define_replace_pattern.sub(r'define("{module}", \1)'.format(module=module), content, re.MULTILINE)
+
+    def get_bundle(self, modules):
+        content = [self.get_bundle_module(module) for module in modules]
+        return '\n'.join(content)
 
     def input(self, **kwargs):
         if self.filename:
-            modules = set(self.get_template_dependencies())
-            print(self.resolve_dependencies(modules, set()))
+            # TODO: work out dependency graph instead of list
+            modules = self.resolve_dependencies(self.get_template_dependencies())
+            bundle = self.get_bundle(modules)
+            # print(bundle)
+            compressor = JsCompressor(content=bundle)
+            output = '\n'.join(compressor.filter_input(forced=True))
+            print(output)
 
             return 'console.log("{}");'.format(self.filename)
         else:
